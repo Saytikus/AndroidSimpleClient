ba0d28063eb73edaf3b30f9dff123f2e46a222e4
Saytikus <k.galkin033@gmail.com>
2026-03-22 12:48:41 +0300

    add app class for project & add module class for presentation & move some dependecies to presentation & add koin compose library with navigation & update lib versions to latest

--- app/presentation/build.gradle.kts
+++ app/presentation/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -28,13 +29,38 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.koin.core)
+    implementation(libs.koin.annotations)
+    implementation(libs.koin.androidx.compose.navigation)
+    implementation(libs.koin.androidx.compose)
+    ksp(libs.koin.ksp.compiler)
 }
--- app/src/main/java/ru/saytikus/androidsimpleclient/AndroidSimpleClient.kt
+++ app/src/main/java/ru/saytikus/androidsimpleclient/AndroidSimpleClient.kt
@@ -1,8 +1,27 @@
 package ru.saytikus.androidsimpleclient
 
+import android.app.Application
 import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
 import org.koin.core.annotation.KoinApplication
+import org.koin.core.annotation.Module
+import org.koin.ksp.generated.module
+import org.koin.ksp.generated.startKoin
 
 @KoinApplication
-@ComponentScan("ru.saytikus")
-class AndroidSimpleClient
+class AndroidSimpleClient : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            modules(PresentationModule().module)
+        }
+    }
+
+}
+
+@Module
+@Configuration
+@ComponentScan("ru.saytikus.presentation")
+class PresentationModule
