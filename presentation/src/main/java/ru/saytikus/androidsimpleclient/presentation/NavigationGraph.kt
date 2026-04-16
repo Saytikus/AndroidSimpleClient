231d333bf7e7a569c00a24ee37c08964cd057e6f
Saytikus <k.galkin033@gmail.com>
2026-04-16 12:13:17 +0300

    add color class & add navigation graph

---
+++ presentation/src/main/java/ru/saytikus/androidsimpleclient/presentation/NavigationGraph.kt
@@ -0,0 +1,25 @@
+package ru.saytikus.androidsimpleclient.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.saytikus.androidsimpleclient.presentation.product.ProductDestination
+import ru.saytikus.androidsimpleclient.presentation.product.ProductRoute
+import ru.saytikus.androidsimpleclient.presentation.product.rememberProductCoordinator
+
+@Composable
+fun NavigationGraph() {
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = ProductDestination
+
+    ) {
+        composable<ProductDestination> {
+            ProductRoute(rememberProductCoordinator())
+        }
+    }
+}
