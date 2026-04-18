4d74cb7bc375e5390f80523a6869bf011902d0df
Saytikus <k.galkin033@gmail.com>
2026-04-18 13:45:41 +0300

    update app version & add version datetime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,9 @@ android {
         applicationId = "ru.saytikus.androidsimpleclient"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
+        val versionDatetime = "13:45:00 18.04.2026"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
