29a4b79510a111a7c1a7042eaecb96dc90233a91
Saytikus <k.galkin033@gmail.com>
2026-04-18 13:32:43 +0300

    add get all products use case impl

--- domain/src/main/java/ru/saytikus/androidsimpleclient/domain/product/useCases/GetAllProductsUseCase.kt
+++ domain/src/main/java/ru/saytikus/androidsimpleclient/domain/product/useCases/GetAllProductsUseCase.kt
@@ -2,14 +2,16 @@ package ru.saytikus.androidsimpleclient.domain.product.useCases
 
 import ru.saytikus.androidsimpleclient.domain.common.dto.MbResult
 import ru.saytikus.androidsimpleclient.domain.common.interfaces.INoCmdInputBoundary
+import ru.saytikus.androidsimpleclient.domain.product.IProductGateway
 import ru.saytikus.androidsimpleclient.domain.product.Product
 
 
 class GetAllProductsUseCase constructor(
+    private val productGateway: IProductGateway
 
 ): INoCmdInputBoundary<MbResult<List<Product>>> {
 
     override suspend fun invoke(cmd: Unit): MbResult<List<Product>> {
-        TODO("Not yet implemented")
+        return productGateway.getUsers()
     }
 }
