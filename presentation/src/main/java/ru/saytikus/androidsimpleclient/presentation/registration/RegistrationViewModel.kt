6a2844b2db3f03755d0236346b7d0c2a4d2836d1
Saytikus <k.galkin033@gmail.com>
2026-04-26 21:51:43 +0300

    add validate input registration data in reg view model & add register profile in reg view model

--- presentation/src/main/java/ru/saytikus/androidsimpleclient/presentation/registration/RegistrationViewModel.kt
+++ presentation/src/main/java/ru/saytikus/androidsimpleclient/presentation/registration/RegistrationViewModel.kt
@@ -5,15 +5,17 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.Named
 import ru.saytikus.androidsimpleclient.domain.common.dto.MbResult
-import ru.saytikus.androidsimpleclient.domain.common.entities.Profile
+import ru.saytikus.androidsimpleclient.domain.common.dto.ValidateResult
+import ru.saytikus.androidsimpleclient.domain.common.profile.Profile
 import ru.saytikus.androidsimpleclient.domain.common.interfaces.IInputBoundary
+import ru.saytikus.androidsimpleclient.domain.common.interfaces.IValidator
 import ru.saytikus.androidsimpleclient.domain.registration.answers.A1RegisterProfileAnswer
 import ru.saytikus.androidsimpleclient.domain.registration.commands.C1RegisterProfileCommand
-import ru.saytikus.androidsimpleclient.presentation.registration.model.ProfileData
 import kotlin.uuid.ExperimentalUuidApi
 
 @KoinViewModel
@@ -25,7 +27,20 @@ class RegistrationViewModel(
 
     @Named("SaveProfileUseCase")
     private val saveProfileCase:
-    IInputBoundary<MbResult<Unit>, Profile>
+    IInputBoundary<MbResult<Unit>, Profile>,
+
+    @Named("UsernameValidator")
+    private val usernameValidator: IValidator<String>,
+
+    @Named("EmailValidator")
+    private val emailValidator: IValidator<String>,
+
+    @Named("PasswordValidator")
+    private val passwordValidator: IValidator<String>,
+
+    @Named("DisplayNameValidator")
+    private val displayNameValidator: IValidator<String>
+
 
 ) : ViewModel() {
 
@@ -35,18 +50,96 @@ class RegistrationViewModel(
 
     val stateFlow: StateFlow<RegistrationState> = _stateFlow.asStateFlow()
 
+
+
+
+    fun onUsernameChanged(newValue: String) {
+        when(val validateResult = usernameValidator.validate(newValue)) {
+            is ValidateResult.Error -> _stateFlow.update {
+                it.copy(
+                    username = newValue,
+                    usernameError = validateResult.error.reasonMessage,
+                )
+            }
+
+            ValidateResult.Success -> _stateFlow.update {
+                it.copy(
+                    username = newValue,
+                    usernameError = null
+                )
+            }
+        }
+    }
+
+    fun onEmailChanged(newValue: String) {
+        when(val validateResult = emailValidator.validate(newValue)) {
+            is ValidateResult.Error -> _stateFlow.update {
+                it.copy(
+                    email = newValue,
+                    emailError = validateResult.error.reasonMessage,
+                )
+            }
+
+            ValidateResult.Success -> _stateFlow.update {
+                it.copy(
+                    email = newValue,
+                    emailError = null
+                )
+            }
+        }
+    }
+
+    fun onPasswordChanged(newValue: String) {
+        when(val validateResult = passwordValidator.validate(newValue)) {
+            is ValidateResult.Error -> _stateFlow.update {
+                it.copy(
+                    password = newValue,
+                    passwordError = validateResult.error.reasonMessage,
+                )
+            }
+
+            ValidateResult.Success -> _stateFlow.update {
+                it.copy(
+                    password = newValue,
+                    passwordError = null
+                )
+            }
+        }
+    }
+
+    fun onDisplayNameChanged(newValue: String) {
+        when(val validateResult = displayNameValidator.validate(newValue)) {
+            is ValidateResult.Error -> _stateFlow.update {
+                it.copy(
+                    displayName = newValue,
+                    displayNameError = validateResult.error.reasonMessage,
+                )
+            }
+
+            ValidateResult.Success -> _stateFlow.update {
+                it.copy(
+                    displayName = newValue,
+                    displayNameError = null
+                )
+            }
+        }
+    }
+
+    fun onRegisterProfileButtonClicked() {
+        registerProfile(stateFlow.value)
+    }
+
     @OptIn(ExperimentalUuidApi::class)
-    fun onRegisterProfileButtonClicked(profileData: ProfileData) {
-        // TODO validate data case
+    private fun registerProfile(state: RegistrationState) {
 
         viewModelScope.launch {
 
             var registerAnswer: A1RegisterProfileAnswer
             val registerResult = registerProfileCase(C1RegisterProfileCommand(
-                profileData.username,
-                profileData.email,
-                profileData.password,
-                profileData.displayName
+                state.username,
+                state.email,
+                state.password,
+                state.displayName
             ))
 
             when(registerResult) {
