b2d2ab8daa2c3c6b5e53520be3820ccc0bb69d76
Saytikus <k.galkin033@gmail.com>
2026-05-03 20:52:25 +0300

    impl chat list view model with chat refresh func

--- presentation/src/main/java/ru/saytikus/androidsimpleclient/presentation/chatList/ChatListViewModel.kt
+++ presentation/src/main/java/ru/saytikus/androidsimpleclient/presentation/chatList/ChatListViewModel.kt
@@ -1,18 +1,67 @@
 package ru.saytikus.androidsimpleclient.presentation.chatList
 
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.StateFlow
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.koin.android.annotation.KoinViewModel
+import org.koin.core.annotation.Named
+import ru.saytikus.androidsimpleclient.domain.chatList.ChatListItem
+import ru.saytikus.androidsimpleclient.domain.common.dto.MbResult
+import ru.saytikus.androidsimpleclient.domain.common.interfaces.IInputBoundary
+import kotlin.uuid.ExperimentalUuidApi
 
+@OptIn(ExperimentalUuidApi::class)
+@KoinViewModel
 class ChatListViewModel(
-    savedStateHandle: SavedStateHandle
+
+    @Named("GetProfileChatsUseCase")
+    private val getChatsCase:
+    IInputBoundary<MbResult<List<ChatListItem>>, Unit>
+
 ) : ViewModel() {
+
 
     private val _stateFlow: MutableStateFlow<ChatListState> = MutableStateFlow(ChatListState())
 
     val stateFlow: StateFlow<ChatListState> = _stateFlow.asStateFlow()
 
 
+
+    init {
+        onChatRefresh()
+    }
+
+    fun onChatRefresh() {
+
+        viewModelScope.launch {
+
+            _stateFlow.update {
+                it.copy(isRefreshing = true)
+            }
+
+            when (val getChatsResult = getChatsCase(Unit)) {
+
+                is MbResult.Failure -> _stateFlow.update {
+                    println(getChatsResult.error)
+                    it.copy(
+                        chats = emptyList(),
+                        isRefreshing = false,
+                        refreshError = true
+                    )
+                }
+
+                is MbResult.Success -> _stateFlow.update {
+                    it.copy(
+                        chats = getChatsResult.response,
+                        isRefreshing = false,
+                        refreshError = false
+                    )
+                }
+            }
+        }
+    }
 }
