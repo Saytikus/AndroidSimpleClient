80220049baa6b06cea2867b8ca407c83d29707b3
Saytikus <k.galkin033@gmail.com>
2026-05-16 16:31:42 +0300

    add init hub api and connect to hub with start app

--- app/src/main/java/ru/saytikus/androidsimpleclient/AndroidSimpleClient.kt
+++ app/src/main/java/ru/saytikus/androidsimpleclient/AndroidSimpleClient.kt
@@ -1,18 +1,23 @@
 package ru.saytikus.androidsimpleclient
 
 import android.app.Application
+import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
+import org.koin.core.Koin
 import org.koin.core.annotation.KoinApplication
 import org.koin.core.logger.Level
 import org.koin.ksp.generated.module
 import org.koin.ksp.generated.startKoin
+import ru.saytikus.androidsimpleclient.data.core.source.remote.signalR.IHubProvider
 import ru.saytikus.androidsimpleclient.di.DataModule
 import ru.saytikus.androidsimpleclient.di.DomainUseCaseModule
 import ru.saytikus.androidsimpleclient.di.DomainValidatorModule
 import ru.saytikus.androidsimpleclient.di.LocalStorageModule
 import ru.saytikus.androidsimpleclient.di.PresentationModule
 import ru.saytikus.androidsimpleclient.di.dataCoreModule
+import ru.saytikus.androidsimpleclient.domain.chat.IChatGateway
+import ru.saytikus.androidsimpleclient.domain.common.message.IMessageGateway
 
 @KoinApplication
 class AndroidSimpleClient : Application() {
@@ -35,7 +40,21 @@ class AndroidSimpleClient : Application() {
                     PresentationModule().module
                 )
             )
+
+            runBlocking {
+                initHubApi(koin)
+            }
         }
+    }
+
+    private suspend fun initHubApi(koin: Koin) {
+
+        val messageGateway = koin.get<IMessageGateway>()
+        val chatGateway = koin.get<IChatGateway>()
+
+        val hubProvider = koin.get<IHubProvider>()
+
+        hubProvider.connect()
     }
 
 }
